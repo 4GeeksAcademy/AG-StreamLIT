9ed0dcdbf4481a30d310a5412fcc34af52b95125
LuneObscure13 <angelagra997@gmail.com>
2026-03-17 12:55:13 +0000

    ML Flask app ready

--- src/app.py
+++ src/app.py
@@ -1,4 +1,41 @@
-from utils import db_connect
-engine = db_connect()
+#-------------Librerias------------
 
-# your code here
+import pickle
+import pandas as pd
+from flask import Flask, render_template, request
+
+
+#=============Flask=================
+
+app=Flask(__name__)
+
+#Cargar el modelo
+model=pickle.load(open("src/model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    platforms = request.form["platforms"]
+    publishers = request.form["publishers"]
+    releaseyear = int(request.form["releaseyear"])
+
+    input_data = pd.DataFrame([{
+        "platforms": platforms,
+        "publishers": publishers,
+        "releaseyear": releaseyear
+    }])
+
+    prediction = model.predict(input_data)[0]
+
+    if prediction == 1:
+        result = "Top Seller 🎮🔥"
+    else:
+        result = "Not a Top Seller"
+
+    return render_template("index.html", prediction_text=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
